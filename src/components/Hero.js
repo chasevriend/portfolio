20cf060caca19c7766d5ea64eae6684aa13f263b
NEWUSER <chasevriend84@gmail.com>
2023-07-18 17:59:53 -0700

    starting hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import '../styles/Hero.css';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import HeroImg from '../images/hero.png';
 
 const Hero = () => {
     return (
         <>
             <div className='hero'>
-                
+                <div className='hero__content'>
+                    <h1>Front-End Developer</h1> 
+                    <p>
+                        Hi, I'm Chase Vriend. I'm a passionate Front-End Developer 
+                        based in Huntington Beach, CA. 🌊 ☀️
+                    </p>
+                    <Link to='/'>
+                        <LinkedInIcon className='hero__icon' />
+                    </Link>
+                    <Link to='/'>
+                        <GitHubIcon className='hero__icon' />
+                    </Link>
+                </div>
+                <img src={HeroImg} alt='picture of Chase' className='hero__img' /> 
             </div>
         </>
     )
